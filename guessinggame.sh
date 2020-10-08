4ae5222094ca3d5b685f2b9f175e517b9fdf4d87
Alfonso Campodonico <alfonso.campodonico@gmail.com>
2020-10-08 11:08:01 -0300

    Input validation

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 echo "Bienvenido al juego de la adivinanza de archivos de Unix"
-numeroarchivos=$(ls -l | wc -l)
-
+numeroarchivos=$(ls -A | wc -l)
 function preguntar { 
-	echo "Intenta adivinar el numero de archivos en la direccion actual"
-    
-	read intento
+	#Comments in english for the approver! Here i check for the input with a message in the same line
+	read -p "Intenta adivinar el numero de archivos en la direccion actual: " intento
 
+	#Validate input, if it's not a number it asks again until a number is in
+	while [[ -n ${intento//[0-9]/} ]] 
+	do
+	    read -p "No es un numero! Intenta otra vez : " intento 
+	done 
 }
-
 preguntar
-while [[ $intento -ne $numeroarchivos ]]
+
+while [[ $intento -ne $numeroarchivos ]] 
 do
 	if [[ $intento -gt $numeroarchivos ]]
 	then
 		echo "Muy arriba"
-		
 	else
 		echo "Muy abajo"
 	fi
 	preguntar
 done
-
 echo "Muy bien! Haz adivinado con exito el numero de archivos!"
